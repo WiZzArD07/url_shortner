b947bf885754a7cb7514d26c0b0ee711645f9415
WiZzArD <arujaiswal12@gmail.com>
2026-04-21 17:24:03 +0530

    port changes

--- config/db.js
+++ config/db.js
@@ -1,29 +1,33 @@
 const mysql = require("mysql2");
 
 let db;
+let isConnected = false;
 
 function connectDB() {
-  return new Promise((resolve, reject) => {
+  return new Promise((resolve) => {
 
     const tryConnect = () => {
 
-      db = mysql.createPool({
+      if (isConnected) return; // ✅ stop further retries
+
+      db = mysql.createConnection({
         host: process.env.DB_HOST,
         user: process.env.DB_USER,
         password: process.env.DB_PASSWORD,
         database: process.env.DB_NAME,
-        port: process.env.DB_PORT || 3306,
-        waitForConnections: true,
-        connectionLimit: 10,
+        port: process.env.DB_PORT,
+        ssl: {
+          rejectUnauthorized: false,
+        },
       });
 
-      db.getConnection((err, connection) => {
+      db.connect((err) => {
         if (err) {
           console.log("DB not ready, retrying in 3 sec...");
           setTimeout(tryConnect, 3000);
         } else {
+          isConnected = true; // ✅ mark connected
           console.log("MySQL Connected");
-          connection.release();
           resolve();
         }
       });
