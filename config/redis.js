7fd988adc8472921ddceeff4bf2613ac43328530
WiZzArD <arujaiswal12@gmail.com>
2026-04-21 17:33:23 +0530

    bug fix

--- config/redis.js
+++ config/redis.js
@@ -5,15 +5,16 @@ let client;
 async function connectRedis() {
   try {
     if (!process.env.REDIS_URL) {
-      console.log("⚠️ No Redis URL provided");
+      console.log("No Redis URL provided");
       return;
     }
 
     client = createClient({
       url: process.env.REDIS_URL,
       socket: {
-        tls: true,
-        rejectUnauthorized: false, 
+        tls: true,                 
+        keepAlive: 5000,
+        reconnectStrategy: (retries) => Math.min(retries * 50, 500),
       },
     });
 
@@ -21,12 +22,18 @@ async function connectRedis() {
       console.error("Redis Error:", err.message);
     });
 
+    client.on("connect", () => {
+      console.log("🔌 Redis connecting...");
+    });
+
+    client.on("ready", () => {
+      console.log("Redis Connected");
+    });
+
     await client.connect();
 
-    console.log("✅ Redis Connected");
-
   } catch (err) {
-    console.error("❌ Redis failed:", err.message);
+    console.error("Redis failed:", err.message);
   }
 }
 
